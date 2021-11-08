7b3d65c8e604f8812c7e3bbc37fb55c2241d80b5
jerry1938 <jerry5585@yahoo.com>
2021-11-08 14:34:15 +0100

    Added a test project and a solution

---
+++ CICD-Uppgift1/Tests/CI_Uppgift1Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CI_Uppgift1Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
