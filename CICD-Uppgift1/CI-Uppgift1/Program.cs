be3a7adb916755815ddc95c3a0d99fd7f3ab9ace
Jangerud <85johjan@gafe.molndal.se>
2021-11-10 14:11:26 +0100

    Added comments to methods in Logic class.

--- CICD-Uppgift1/CI-Uppgift1/Program.cs
+++ CICD-Uppgift1/CI-Uppgift1/Program.cs
@@ -10,6 +10,7 @@ namespace CI_Uppgift1
         {
             Admin admin = new Admin("Admin", 30000, "admin1", "admin1234", true);
             User jangen = new User("Peon", 20000, "Johan", "Johan123", false);
+            logicClass.CreateUserWithJson(jangen);
             //logicClass.CreateUser(jangen);
             //logicClass.CreateUser(admin);
             //logicClass.GetUser(jangen.Username);
