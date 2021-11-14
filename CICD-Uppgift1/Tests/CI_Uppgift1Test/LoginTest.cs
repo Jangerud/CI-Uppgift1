0b9025a8482c10711605427e9813352ba874940f
jerry1938 <jerry5585@yahoo.com>
2021-11-14 15:25:52 +0100

    Fixed merge conflicts.

--- CICD-Uppgift1/Tests/CI_Uppgift1Test/LoginTest.cs
+++ CICD-Uppgift1/Tests/CI_Uppgift1Test/LoginTest.cs
@@ -1,22 +1,14 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CI_Uppgift1;
 using System.Collections.Generic;
-<<<<<<< HEAD
-=======
 using System;
 using System.IO;
->>>>>>> login
 
 namespace CI_Uppgift1Test
 {
     [TestClass]
     public class LoginTest
     {
-<<<<<<< HEAD
-        [TestMethod]
-        public void Login_UserDoesNotExist_ReturnsFalse(){
-            Assert.IsFalse(new Logic().Login("abc", "123"));
-=======
         private Logic logic;
         private string filePath;
 
@@ -36,34 +28,21 @@ namespace CI_Uppgift1Test
         [TestMethod]
         public void Login_UserDoesNotExist_ReturnsFalse(){
             Assert.IsFalse(logic.Login("abc", "123"));
->>>>>>> login
         }
 
         [TestMethod]
         public void Login_UserDoesExist_ReturnTrue(){
-<<<<<<< HEAD
-            Assert.IsTrue(new Logic().Login("user1", "123"));
-=======
             Assert.IsTrue(logic.Login("user1", "123"));
->>>>>>> login
         }
 
         [TestMethod]
         public void Login_WrongPassword_ReturnFalse(){
-<<<<<<< HEAD
-            Assert.IsFalse(new Logic().Login("user1", "abc"));
-=======
             Assert.IsFalse(logic.Login("user1", "abc"));
->>>>>>> login
         }
 
         [TestMethod]
         public void Login_CorrectPassword_ReturnTrue(){
-<<<<<<< HEAD
-            Assert.IsTrue(new Logic().Login("user1", "123"));
-=======
             Assert.IsTrue(logic.Login("user1", "123"));
->>>>>>> login
         }
     }
 }
