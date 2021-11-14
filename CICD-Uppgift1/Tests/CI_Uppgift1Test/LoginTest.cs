7748d6aeb4d08d3ba2f81471d20e852fae10111b
jerry1938 <jerry5585@yahoo.com>
2021-11-14 14:52:29 +0100

    Added TestInitialize and TestCleanup.

--- CICD-Uppgift1/Tests/CI_Uppgift1Test/LoginTest.cs
+++ CICD-Uppgift1/Tests/CI_Uppgift1Test/LoginTest.cs
@@ -1,15 +1,32 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CI_Uppgift1;
 using System.Collections.Generic;
+using System;
+using System.IO;
 
 namespace CI_Uppgift1Test
 {
     [TestClass]
     public class LoginTest
     {
+        private Logic logic;
+        private string filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        [TestInitialize]
+        public void TestInitialize(){
+            this.logic = new Logic();
+            logic.SerializeData(logic.CreateDummyData(), filePath + "/test.json");
+        }
+
+        [TestCleanup]
+        public void TestCleanup(){
+            this.logic = null;
+            File.Delete(filePath + "/test.json");
+        }
+
         [TestMethod]
         public void Login_UserDoesNotExist_ReturnsFalse(){
-            Assert.IsFalse(new Logic().Login("abc", "123"));
+            Assert.IsFalse(logic.Login("abc", "123"));
         }
 
         [TestMethod]
