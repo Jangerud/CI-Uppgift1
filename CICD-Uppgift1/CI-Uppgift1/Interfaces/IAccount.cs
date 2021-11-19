e35de6188eb7bcbce5bf4889cc9b8a0ba24673e7
Jangerud <85johjan@gafe.molndal.se>
2021-11-19 11:23:23 +0100

    Added comments to the IAccount class properties.

--- CICD-Uppgift1/CI-Uppgift1/Interfaces/IAccount.cs
+++ CICD-Uppgift1/CI-Uppgift1/Interfaces/IAccount.cs
@@ -2,12 +2,35 @@ namespace CI_Uppgift1.Interfaces
 {
     public interface IAccount
     {
+        /// <summary>
+        /// Property for the salary of the user.
+        /// </summary>
+        /// <value>Can be set aswell as gotten when called upon.</value>
         public int Salary { get; set; }
+
+        /// <summary>
+        /// Property for the title of the user.
+        /// </summary>
+        /// <value>Can be set aswell as gotten when called upon.</value>
         public string Title { get; set; }
+
+        /// <summary>
+        /// Property for the username of the user.
+        /// </summary>
+        /// <value>Can be set aswell as gotten when called upon.</value>
         public string Username { get; set; }
+
+        /// <summary>
+        /// Property for the password of the user.
+        /// </summary>
+        /// <value>Can be set aswell as gotten when called upon.</value>
         public string Password { get; set; }
 
-        public bool IsAdmin {get; set; }
+        /// <summary>
+        /// Property for the user regarding if they are an admin or not.
+        /// </summary>
+        /// <value>Can be set aswell as gotten when called upon.</value>
+        public bool IsAdmin { get; set; }
 
     }
 }
